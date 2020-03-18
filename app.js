7f007a6e75c9a4158ddb32ec84e455b214ab114f
Andrew Lapshov <titan1um@yandex.ru>
2020-03-18 12:51:50 +0300

    auth changed

--- app.js
+++ app.js
@@ -32,22 +32,24 @@ mongoose.connect(MONGO_IP, {
 app.use(limiter);
 app.use(requestLogger);
 
-app.use(
-  cors({
-    origin(origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
-        const msg =
-          'The CORS policy for this site does not ' +
-          'allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  }),
-);
+// app.use(
+//   cors({
+//     origin(origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+//         const msg =
+//           'The CORS policy for this site does not ' +
+//           'allow access from the specified Origin.';
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   }),
+// );
+
+app.use(cors());
 
 app.use(helmet());
 
